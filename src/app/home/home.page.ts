420dd03f7b32338fe633c4033ccd4125f53ef543
Eric Clota Ortiz <eclota@eio.cat>
2024-11-04 13:50:09 +0100

    Fer que llegeixi codis de barra amb ml/kit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { UtilsGeneralsService } from '../services/utils-generals.service';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { Camera, CameraResultType } from '@capacitor/camera';
+import { BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-home',
@@ -40,4 +41,27 @@ export class HomePage {
     }
 
   }
+
+  async scanBarcode() {
+    // Sol·licitar permisos de càmera
+    const permission = await BarcodeScanner.requestPermissions();
+    if (permission.camera === 'granted') {
+      // Iniciar l'escàner de codis de barres
+      const result = await BarcodeScanner.scan();
+  
+      // Comprovar que hi hagi codis llegits
+      if (result.barcodes && result.barcodes.length > 0) {
+        // Accedir al primer codi llegit
+        const barcode = result.barcodes[0]; // Aquí accedim correctament al primer element
+        console.log('Codi llegit:', barcode.displayValue); // Mostrar el valor llegit
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: barcode.displayValue, life: 3000 });
+        // Aquí pots fer el que necessitis amb el codi llegit
+      } else {
+        console.log('No s\'han llegit codis de barres.');
+      }
+    } else {
+      console.log('Permís de càmera denegat');
+    }
+  }
+  
 }
